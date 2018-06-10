6b016df6439fe76f05f7673a984c7692ba2b0d03
Andrea Straforini <aspem2015@gmail.com>
2018-06-10 12:19:58 +0200

    Add files via upload

---
+++ threadPool.h
@@ -0,0 +1,30 @@
+#ifndef _THREADPOOL_H
+#define _THREADPOOL_H
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<pthread.h>
+#include<stdbool.h>
+#include"debug/debug.h"
+#include"libhl/linklist.h"
+#include<errno.h>
+#include<math.h>
+
+typedef struct _threadPool* thread_pool_t;
+typedef struct _future* future_t;
+
+thread_pool_t create_thread_pool(int numberOfExecutors);
+void start_thread_pool(thread_pool_t tp);
+
+future_t add_job_head(thread_pool_t tp,const pthread_attr_t *attr,void *(*start_routine)(void*),void *arg);
+future_t add_job_tail(thread_pool_t tp,const pthread_attr_t *attr,void *(*start_routine)(void*),void *arg);
+bool is_ready(future_t future);
+void* future_get(future_t future);
+
+//Testing necessary
+void destroy_now_thread_pool(thread_pool_t threadPool);
+
+
+void destroy_thread_pool(thread_pool_t threadPool);
+void destroy_future(future_t future );
+#endif
