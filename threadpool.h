df9c877267db27a31b9638e7f686aa8557782318
strafoxultrapowa <aspem2015@gmail.com>
2019-02-17 21:03:46 +0100

    added comments

--- threadpool.h
+++ threadpool.h
@@ -105,8 +105,8 @@ thread_pool_t* create_fixed_size_thread_pool(int size,const pthread_attr_t *attr
 
 
 /**
- *
- * @param thread_pool
+ * This function frees the memory occupied by the threadpool.Use one of the shutdown functions before using "destroy_threadpool".
+ * @param thread_pool (if null reference no action is performed)
  */
 void destroy_thread_pool(thread_pool_t* thread_pool);
 
@@ -128,7 +128,7 @@ enum thread_pool_state{
 };
 
 /**
- *
+ * Set the threadpool passed in the state: THREAD_POOL_RUNNING
  * @param tp
  * @return 0 if successful, -1 if tp is a null reference
  */
@@ -136,7 +136,7 @@ int start_thread_pool(thread_pool_t* tp);
 
 
 /**
- *
+ * Set the threadpool passed in the state: THREAD_POOL_PAUSED
  * @param tp
  * @return 0 if successful, -1 if tp is a null reference
  */
@@ -147,20 +147,26 @@ int pause_thread_pool(thread_pool_t* tp);
  *
  * @param thread_pool
  */
-int shut_down_now_thread_pool(thread_pool_t* thread_pool);//todo
+int shut_down_now_thread_pool(thread_pool_t* thread_pool);//todo tobe implemented
 
 
 /**
- *
+ * Set the threadpool passed in the state: THREAD_POOL_STOPPED.
+ * The function "gently" interrupts the threadpool by waiting for all threads to finish their tasks.
+ * The termination of the function is not guaranteed if the threadpool threads remain blocked.
+ * After this function call the threadpool is unusable (use destroy_thread_pool () to free the memory)
  * @param thread_pool
  * @return 0 if successful, -1 if tp is a null reference
  */
 int shut_down_thread_pool(thread_pool_t* thread_pool);
 
 /**
+ * This function allows to obtain the current threadpool status.
  * @param thread_pool
  * @return the threadpool state {THREAD_POOL_STOPPED=0,THREAD_POOL_RUNNING=1,THREAD_POOL_PAUSED=2} if successful
  * @return THREAD_POOL_ERROR if tp is a null reference
+ * @note Warning: the status reflects the threadpool situation during the function call.
+ * It is not assured that after the call to the function the state remains in that particular situation.
  */
 enum thread_pool_state get_thread_pool_state(thread_pool_t* tp);
 
@@ -174,21 +180,23 @@ enum thread_pool_state get_thread_pool_state(thread_pool_t* tp);
  * ************************************/
 
 /**
- *
- * @param tp
- * @param start_routine
- * @param arg
- * @return
- */
+ * This function adds the task to the head of the jobs list.
+ * @param tp the threadpool on which to add the task
+ * @param start_routine the pointer to the function to be performed
+ * @param arg the parameters to be passed to the function start_routine
+ * @return returns a pointer to the future structure, which has as a payload a pointer to the return value of the function.
+ * @return null if tp or start_routine are invalid pointers.
+*/
 future_t* add_job_head(thread_pool_t* tp,void *(*start_routine)(void*),void *arg);
 
 /**
- *
- * @param tp
- * @param start_routine
- * @param arg
- * @return
- */
+ * This function adds the task to the tail of the jobs list.
+ * @param tp the threadpool on which to add the task
+ * @param start_routine the pointer to the function to be performed
+ * @param arg the parameters to be passed to the function start_routine
+ * @return returns a pointer to the future structure, which has as a payload a pointer to the return value of the function.
+ * @return null if tp or start_routine are invalid pointers.
+*/
 future_t* add_job_tail(thread_pool_t* tp,void *(*start_routine)(void*),void *arg);
 
 
